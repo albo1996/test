ba5de7542e53a74ac941ac4d286afec6ec01ff7a
Alexander Borovik <borovikav96@gmail.com>
2021-01-09 03:01:50 +0700

    commit #1

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: main.o age.o
+	gcc main.o age.o -o main
+main.o: main.c
+	gcc -c main.c
+age.o: age.c
+	gcc -c age.c
+clear:
+	rm main
+	rm *.o
--- age.c
+++ age.c
@@ -5,4 +5,6 @@ void age(int year)
 	int current_year = 2021;
 
 	printf("Возраст: %d\n", current_year - year);
+
+	printf("******************************************\n");
 }
